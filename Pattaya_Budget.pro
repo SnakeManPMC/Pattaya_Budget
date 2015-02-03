e2b121cbabf454468902e77bd755163e27e1c0d8
Snake Man <snakeman@phnet.fi>
2015-02-03 04:49:18 +0200

    initial commit

---
+++ Pattaya_Budget.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-11-14T14:01:39
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Pattaya_Budget
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        widget.cpp
+
+HEADERS  += widget.h
+
+FORMS    += widget.ui
---
+++ widget.cpp
@@ -0,0 +1,27 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_update_Button_clicked()
+{
+	int eurobudget = (ui->days->value() * (ui->hotel->value() + ui->food->value() + ui->taxi->value() + ui->bars->value() + ui->nartut->value()) / ui->exchange->value() + ui->airlineTicket->value() );
+    // EURO sum
+    ui->sum_euro_Label->setText(QString::number(eurobudget));
+
+    // BAHT sum
+    ui->sum_baht_Label->setText(QString::number((ui->days->value() * (ui->hotel->value() + ui->food->value() + ui->taxi->value() + ui->bars->value() + ui->nartut->value())) + (ui->airlineTicket->value() * ui->exchange->value())));
+
+    // monthly savings
+    ui->sum_months_label->setText(QString::number(eurobudget / ui->monthlySavings->value()));
+}

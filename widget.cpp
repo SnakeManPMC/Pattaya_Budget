fbda6eaa0324c0ab347b6feb2dc8bbc802a17d67
Snake Man <snakeman@phnet.fi>
2015-02-14 19:09:42 +0200

    Formatting, hehe

--- widget.cpp
+++ widget.cpp
@@ -2,26 +2,27 @@
 #include "ui_widget.h"
 
 Widget::Widget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::Widget)
+	QWidget(parent),
+	ui(new Ui::Widget)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 }
 
 Widget::~Widget()
 {
-    delete ui;
+	delete ui;
 }
 
 void Widget::on_update_Button_clicked()
 {
 	int eurobudget = (ui->days->value() * (ui->hotel->value() + ui->food->value() + ui->taxi->value() + ui->bars->value() + ui->nartut->value()) / ui->exchange->value() + ui->airlineTicket->value() );
-    // EURO sum
-    ui->sum_euro_Label->setText(QString::number(eurobudget));
 
-    // BAHT sum
-    ui->sum_baht_Label->setText(QString::number((ui->days->value() * (ui->hotel->value() + ui->food->value() + ui->taxi->value() + ui->bars->value() + ui->nartut->value())) + (ui->airlineTicket->value() * ui->exchange->value())));
+	// EURO sum
+	ui->sum_euro_Label->setText(QString::number(eurobudget));
 
-    // monthly savings
-    ui->sum_months_label->setText(QString::number(eurobudget / ui->monthlySavings->value()));
+	// BAHT sum
+	ui->sum_baht_Label->setText(QString::number((ui->days->value() * (ui->hotel->value() + ui->food->value() + ui->taxi->value() + ui->bars->value() + ui->nartut->value())) + (ui->airlineTicket->value() * ui->exchange->value())));
+
+	// monthly savings
+	ui->sum_months_label->setText(QString::number(eurobudget / ui->monthlySavings->value()));
 }
